96d19ab068459571880d6f29c0c1e765f179a3a8
오성환 <oseonghwan@hwanyMBP11.local>
2023-11-09 04:26:04 +0900

    모든 클래스 및 기능 구현 완료
    모든 기능을 구현 완료 하였고 디버깅이 쉽도록 콘솔에 이모지로 프린트하도록 설정함
    
    추후 변경 필요:
    - 회전/전진하기 전에 센싱 먼저 하도록 시퀀스 변경 필요
    - (row,col) 순으로 작성했는데 과제 pdf에 맞춰 (col, row)로 변경 필요
    
    원래 계획(설계)과 달라진 점:
    - SIMController를 addon이라고 생각. SIMController가 시스템의 진입점으로, 필요한 모든 객체 생성
        -> public 속성 mapObject = Map() 생성
    - RobotController의 __path 삭제. SIMController가 명령을 내리기만 하므로 불필요
    - RobotController의 detectPosition() 메서드 추가
    - PositionSensor의 getCurrentPosition() -> detectPosition()으로 이름 변경

--- Robot_Control_and_Monitoring/SIMController.py
+++ Robot_Control_and_Monitoring/SIMController.py
@@ -1,4 +1,13 @@
+from Path_Planning_and_Map_Management.PathPlanner import PathPlanner
+from Robot_Control_and_Monitoring.RobotController import RobotController
+
+
 class SIMController:
+    def __init__(self, pathPlanner: PathPlanner, robotController: RobotController):
+        self.__pathPlanner = pathPlanner
+        self.__robotController = robotController
+
+    # 이동 명령을
     def sendMovementCommand(self, command):
         """이동 명령을 시뮬레이터에 전송"""
         # 명령 전송 로직 구현
