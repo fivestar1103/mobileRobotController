9f558c26de1eadfd05ecb83730087bc7a1684830
fivestar1103 <fivestar1103@uos.ac.kr>
2023-11-09 23:30:30 +0900

    branch commit test

--- Data_Structures/Hazard.py
+++ Data_Structures/Hazard.py
@@ -1,17 +1,17 @@
 class Hazard:
     def __init__(self, col, row, hidden=True):
-        self.position = (col, row)
-        self.hidden = hidden
+        self.__position = (col, row)
+        self.__hidden = hidden
 
     def getPosition(self):
-        return self.position
+        return self.__position
 
     def isHidden(self):
-        return self.hidden
+        return self.__hidden
 
     def setRevealed(self):
-        self.hidden = False
+        self.__hidden = False
         print(f"⚠️ Hazard at {self.getPosition()} was revealed!")
 
     def __str__(self):
-        return f"Hazard({self.position}, Hidden: {self.hidden})"
+        return f"Hazard({self.__position}, Hidden: {self.__hidden})"
